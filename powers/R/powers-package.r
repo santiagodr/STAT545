9a6e78d7bb611a220b40274b9bc1a1299349b538
Santiago David <sdrrock@gmail.com>
2017-11-23 11:00:14 -0800

    cm109 part 1 and 2 - package updated

---
+++ powers/R/powers-package.r
@@ -0,0 +1,11 @@
+#' powers.
+#'
+#' This package is just an exercise for STAT547
+#' It allows you to power any number to a square term
+#'
+#'
+#' @name powers
+#' @author Santiago David
+#' @note I will expand this package to allow user use cubic terms
+#' @docType package
+NULL
